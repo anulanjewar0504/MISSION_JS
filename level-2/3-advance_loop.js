782bd1fffb0e90bb61e0ba19936d54b3c46afbc4
Anurag Lanjewar <85513169+anulanjewar0504@users.noreply.github.com>
2023-12-19 17:11:21 +0530

    Update 3-advance_loop.js

--- level-2/3-advance_loop.js
+++ level-2/3-advance_loop.js
@@ -4,4 +4,23 @@
 a = [1,2,3,4,5,6,7,8,9,12,32,45,65,78,98];
 a.forEach(function(val){
      console.log(val);
-});
+}); 
+// This loop dose not make change in original copy of array but it creates temporary copy of that array. 
+
+
+
+// forin loop 
+// The for...in loop in JavaScript is a way to iterate over the properties of an object or other iterable object. 
+// It runs a block of code once for each property in the object, providing access to the property name and value within the loop.
+
+var obj = {
+     name: "Anurag",
+     age: 23,
+     city: "Pune"
+}
+for (var key in obj){
+     console.log(key);
+     console.log(obj);
+     console.log(obj[key]);
+     console.log(key,":", obj[key]);
+}
